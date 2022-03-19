4f1e4b01f9a38b963bd6e763154bf2a9ed503360
karimbedu <kpurushotham.777@gmail.com>
2022-03-19 08:05:33 +0530

    Surya project added in Sekhar
    Surya project added in Sekhar...

---
+++ src/sunitha/Sikuli.java
@@ -0,0 +1,47 @@
+package sunitha;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Screen;
+import org.sikuli.script.SikuliException;
+
+public class Sikuli {
+
+	//static 
+//	static WebDriver driver;
+
+	public static void main(String[] args) throws SikuliException, InterruptedException {
+		Screen s = new Screen();
+		Thread.sleep(5000);
+		s.dragDrop("E:\\Selenium\\Sikuli\\Recyclebin.png","E:\\Selenium\\Sikuli\\Windowsicon.png");
+		/*
+		 * s.find("E:\\Selenium\\Sikuli\\Recyclebin.png");
+		 * System.out.println("Recyclebin source image found");
+		 * s.find("E:\\Selenium\\Sikuli\\Windowsicon.png");
+		 * System.out.println("Windowsicon target image found");
+		  s.dragDrop("E:\\Selenium\\Sikuli\\Recyclebin.png",
+		  "E:\\Selenium\\Sikuli\\Windowsicon.png");
+		 * System.out.println("Drag Drop sikuli successfully worked");
+		 */
+		//		 s.click("E:\\Selenium\\Sikuli\\paint.PNG");
+		//
+		//  s.click("E:\\Selenium\\Sikuli\\Wify.PNG");
+
+
+	//	System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\chromedriver.exe");  
+		//driver=new ChromeDriver();
+		//driver.manage().window().maximize();
+
+			//	driver.get("http://seleniumlearn.com/java");
+		//	s.click("E:\\Selenium\\Sikuli\\Search.PNG");
+		//Thread.sleep(3000);
+		//s.type("E:\\Selenium\\Sikuli\\Search.PNG", "Purushotham Karimbedu");
+		//Thread.sleep(3000);
+		//s.click("E:\\Selenium\\Sikuli\\Search-Botton.JPG");
+
+
+
+	}
+
+}
